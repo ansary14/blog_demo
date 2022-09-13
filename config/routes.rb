01b10f0fddc74e1dc4b51c56552c87e0a34f75d0
Ayub Ansary <ayubuap35@gmail.com>
2022-09-13 16:08:18 +0600

    root path as a home page and change abouth path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # home page will be rendered in root directory ; example: localhost:3000
+  root "pages#home"
+  # about page will be rendered in about directory ; example: localhost:3000/about
+  get 'about', to: 'pages#about'
 end

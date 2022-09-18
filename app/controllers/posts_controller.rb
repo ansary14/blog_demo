c2f73d0dbd2137107b42dde58767dec2bda7f779
Ayub Ansary <ayubuap35@gmail.com>
2022-09-19 01:05:59 +0600

    Update the views counter by calling show action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,8 @@ class PostsController < ApplicationController
 
   # GET /posts/1 or /posts/1.json
   def show
+    # update post views count
+    @post.update(views: @post.views + 1)
   end
 
   # GET /posts/new

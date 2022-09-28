1bd11852e89d6b7f66e5a16dc86feb014992120b
Ayub Ansary <ayubuap35@gmail.com>
2022-09-28 21:28:23 +0600

    Update method name in singular format | Define Update action for comment update functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
   before_action :set_post
 
   def create
-    @comment = @post.comments.create(comments_params)
+    @comment = @post.comments.create(comment_params)
     @comment.user = current_user
     if @comment.save
       flash[:notice] = 'Comment created successfully!'
@@ -22,13 +22,23 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def update
+    @comment = @post.comments.find(params[:id])
+    if @comment.update(comment_params)
+      flash[:notice] = 'Comment updated successfully'
+    else
+      flash[:alert] = 'Comment has not been updated!'
+    end
+    redirect_to post_path(@post)
+  end
+
   private
 
   def set_post
     @post = Post.find(params[:post_id])
   end
 
-  def comments_params
+  def comment_params
     params.require(:comment).permit(:body)
   end
 end
